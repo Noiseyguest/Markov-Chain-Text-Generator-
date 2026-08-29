bc761def7a70dc6787b7d7c5059b58971c74c1dc
Noiseyguest <72784127+Noiseyguest@users.noreply.github.com>
2026-08-29 19:34:59 +0900

    Enhance comments in markov_chain.py for clarity
    Added comments to clarify the purpose of key lines in the Markov chain text generation.

--- markov_chain.py
+++ markov_chain.py
@@ -18,9 +18,9 @@ def generate_text(chain, start_word, order, length=100):
     result = list(current_word) #easy way of forming the overall words by appending them to a list first and then converting them into a string later
 
     for x in range(length-1):
-        if current_word not in chain:
+        if current_word not in chain: #breaks the text generation if new tuple no longer matches with tuples which are key fields
             break   
-        next_word = random.choice(chain[current_word])
+        next_word = random.choice(chain[current_word]) #randomly chooses one of many words associated with that particular key field tuple
         result.append(next_word)
         current_word = tuple(result[-order:]) #elements of the tuple should be equal to elements of the tuple which are key fields
     
